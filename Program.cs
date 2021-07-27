ec8b38e775a6e418ae4edb75909f82f3cf26b741
Mehdi Amiri <3nz0f3rrari@gmail.com>
2021-07-27 19:58:26 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace InventoryManagementApplication
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
